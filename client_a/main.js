07c5c7cbc5c96a924d5c13819ae0fb2e3e2bdaa7
frogBear_7 <{220098tt@yse-c.net}>
2023-11-08 11:30:56 +0900

    up

--- client_a/main.js
+++ client_a/main.js
@@ -1,5 +1,11 @@
+// モジュール読み込み
 const message = require('message')
 const dotenv = require('dotenv')
 const fs = require('fs')
 
 dotenv.config()
+
+// dotenvの設定読み込み
+const NAME = process.env.NAME
+const NUMBER = process.env.NUMBER
+
