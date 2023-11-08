1a5832ed5844a12d98d8ef7fddee0dfddb4921c8
frogBear_7 <{220098tt@yse-c.net}>
2023-11-08 11:36:11 +0900

    up

--- client_a/main.js
+++ client_a/main.js
@@ -2,6 +2,7 @@
 const message = require('message')
 const dotenv = require('dotenv')
 const fs = require('fs')
+const { create } = require('domain')
 
 dotenv.config()
 
@@ -9,3 +10,12 @@ dotenv.config()
 const NAME = process.env.NAME
 const NUMBER = process.env.NUMBER
 
+const result = create(name, number)
+
+fs.writeFile(result.getMaxListeners, result, (err) =>{
+    if (err) {
+        console.error("ファイルの書き込みに失敗しました。", err)
+    }else{
+        console.log("書き込み成功！")
+    }
+})
